619099c07342a45f6609a34990649e09a1c3cccb
Cota <cotarelorodrigo@gmail.com>
2023-09-25 20:48:17 -0300

    edit clan details dialog

--- src/Components/Dialogs/Gameplay/Clan/clan-list.jsx
+++ src/Components/Dialogs/Gameplay/Clan/clan-list.jsx
@@ -1,52 +1,60 @@
 import './clan-list.scss'
 import AoDialog from "../../../Common/ao-dialog/ao-dialog";
 import AoButton from '../../../Common/ao-button/ao-button';
+import ClanDetailsDialog from './clan-details-dialog';
 import { useState, useRef } from 'react';
 
-const mockClanList = [
-  'Two Easy',
-  'mRades',
-  'clan loco',
-  'el leon',
-  'patito bullrich',
-  'el indio panquequen',
-  'dolar',
-  'dolar qatar',
-  'dolar ccl',
-  'dolar mep',
-  'dolar bolsa',
-  'dolar qatar',
-  'DANGER',
-  'Skywalker',
-  'SayHEllo',
-  'PUESTO AHI',
-  'Daleeee'
-]
+const Alineaciones = {
+  'neutral': 'Neutral',
+  'armada': 'Armada',
+  'legion': 'Legion'
+}
 
-/*
-<input ref={inputRef} type='text' placeholder='Busca tu clan' onChange={(e) => setSearch(e.target.value)} value={search}/>
-
-{search ?
-                  <img 
-                    alt='search' 
-                    src={require(`../../../../assets/Icons/Dialogs/cancel.png`)}
-                  />
-                  :
-                  <img alt='search' src={require(`../../../../assets/Icons/Dialogs/glass.png`)} sizes='2px 2px'/>
-                }
-*/
-
-export default function ClanListDialog({styles}) {
+export default function ClanListDialog({styles, stylesDetail}) {
   const [search, setSearch] = useState('')
+  const [alineacion, setAlineacion] = useState(Alineaciones.neutral)
   const inputRef = useRef(null);
+  const [selectedClan, setSelectedClan] = useState(false)
+
+  if(selectedClan){
+    return (
+      <ClanDetailsDialog 
+        clan={selectedClan} 
+        styles={stylesDetail} 
+        onClose={() => setSelectedClan(null)}
+      />
+    )
+  }
+
+  //TO DO: replace this FN
+  const mockGetDetailClanFn = async (clanName, alineacion) => {
+    return {
+      nombre: clanName,
+      alineacion: alineacion,
+      ...mockClanDetail
+    }
+  }
+
+  let clanes = search ? mockClanList.filter((clan) => clan.nombre.toLocaleLowerCase().includes(search)) : mockClanList
+  let styleAlineacion = ''
+  console.log('Alineacion:', alineacion)
+  if(alineacion === Alineaciones.legion) {
+    clanes = clanes.filter((clan) => clan.alineacion === Alineaciones.legion)
+    styleAlineacion = ' red'
+  }
+  if(alineacion === Alineaciones.armada) {
+    clanes = clanes.filter((clan) => clan.alineacion === Alineaciones.armada)
+    styleAlineacion = ' blue'
+  }
 
   return (
     <AoDialog styles={styles}>
       <div className="clan-list-dialog">
         <h2>CLANES</h2>
-        <div className='list-box'>  
+        <img className='frame-sep' src={require('../../../../assets/frame/top-center.jpg')} />
+        <div className='list-box'>
           <div className='filters'>
-            <div className='search'>
+          <div className='btn search'>
               <input ref={inputRef} type='text' placeholder='Busca tu clan' onChange={(e) => setSearch(e.target.value)} value={search}/>
               {search ?
                   <img 
@@ -63,15 +71,119 @@ export default function ClanListDialog({styles}) {
                   :
                   <img alt='search' src={require(`../../../../assets/Icons/Dialogs/glass.png`)} sizes='2px 2px'/>
                 }
+                </div>
+            <div className='select'>
+            <label for='Alineacion' >Alineación</label>
+              <select 
+                id='Alineacion' 
+                className="btn alineacion" 
+                name="alineacion"
+                value={alineacion}
+                onChange={(e) => setAlineacion(e.target.value)}
+              >
+              <option value="">Todas</option>
+                <option value={Alineaciones.legion}>Legión Oscura</option>
+                <option value={Alineaciones.armada}>Armada</option>
+              </select>
             </div>
-            
           </div>
-          <div className='list'>
-            {mockClanList.map((clan) => <span>{clan}</span>)}
+          <div className={'list'+ styleAlineacion}>
+            {clanes.map((clan) => 
+              <span 
+                onClick={() => {
+                  mockGetDetailClanFn(clan.nombre, clan.alineacion).then((clanDetail) => setSelectedClan(clanDetail))
+                }}
+              >
+                {clan.nombre}
+              </span>)
+            }
           </div>
         </div>
-        <AoButton caption='guardar' styles='guardar'>FUNDAR CLAN</AoButton>
-      </div>
-    </AoDialog>
+        <img className='frame-sep small' src={require('../../../../assets/frame/top-center.jpg')} />
+        <div className='last'>
+          <AoButton caption='guardar' styles='fundar'>FUNDAR CLAN</AoButton>
+        </div>
+      </div> 
+    </AoDialog>  
   )
+}
+
+
+const mockClanList = [
+  {
+    nombre: 'Two Easy',
+    alineacion: Alineaciones.neutral
+  },
+  {
+    nombre: 'mRades',
+    alineacion: Alineaciones.neutral
+  },
+  {
+    nombre: 'clan loco',
+    alineacion: Alineaciones.neutral
+  },
+  {
+    nombre: 'el leon',
+    alineacion: Alineaciones.armada
+  },
+  {
+    nombre: 'patito bullrich',
+    alineacion: Alineaciones.neutral
+  },
+  {
+    nombre: 'el indio panquequen',
+    alineacion: Alineaciones.legion
+  },
+  {
+    nombre: 'dolar',
+    alineacion: Alineaciones.armada
+  },
+  {
+    nombre: 'dolar qatar',
+    alineacion: Alineaciones.legion
+  },
+  {
+    nombre: 'dolar ccl',
+    alineacion: Alineaciones.armada
+  },
+  {
+    nombre: 'dolar mep',
+    alineacion: Alineaciones.neutral
+  },
+  {
+    nombre: 'dolar bolsa',
+    alineacion: Alineaciones.legion
+  },
+  {
+    nombre: 'dolar qatar',
+    alineacion: Alineaciones.legion
+  },
+  {
+    nombre: 'DANGER',
+    alineacion: Alineaciones.legion
+  },
+  {
+    nombre: 'Skywalker',
+    alineacion: Alineaciones.armada
+  },
+  {
+    nombre: 'SayHEllo',
+    alineacion: Alineaciones.neutral
+  },
+  {
+    nombre: 'PUESTO AHI',
+    alineacion: Alineaciones.legion
+  },
+  {
+    nombre: 'Daleeee',
+    alineacion: Alineaciones.neutral
+  }
+]
+
+const mockClanDetail = {
+  'fundador': 'tenesule',
+  'fecha_creacion': '03/08/2023',
+  'lider': '1269',
+  'miembros': 10,
+  'nivel_del_clan': 4
 }
